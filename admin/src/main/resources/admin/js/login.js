e5c0638a7866d8e6d53471b9b38da32342d06537
RyanSusana <ryansusana.com>
2018-05-31 19:43:54 +0200

    REBRANDING YEAHHHHHHHHHHHHHHHHHH -new logo -new UI

---
+++ admin/src/main/resources/admin/js/login.js
@@ -0,0 +1,21 @@
+$(document).on('submit', '#login-form', function (event) {
+
+
+    event.preventDefault();
+    axios({
+        method: 'post',
+        url: "/login",
+        data: $("#login-form").serialize()
+    }).then(function (response) {
+        //console.log(response.data)
+        //ref.modelData = response.data
+        console.log("redirecting");
+        window.location = response.data
+
+    })
+        .catch(function (error) {
+            console.log(error.response.status);
+            UIkit.notification(error.response.data, {status: 'danger'})
+
+        });
+});
