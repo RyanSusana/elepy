69256c6b79725c087fcdab1d902f479b2e8661c0
RyanSusana <ryansusana.com>
2018-06-03 09:07:52 +0200

    For GeneratedFields

--- core/src/main/java/com/elepy/annotations/Text.java
+++ core/src/main/java/com/elepy/annotations/Text.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD})
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface Text {
     TextType value() default TextType.TEXTFIELD;
 
