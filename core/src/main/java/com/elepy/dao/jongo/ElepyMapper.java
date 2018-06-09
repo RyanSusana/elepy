a5798a7715c08450f01cc3d25079a0b84d9b8ac6
RyanSusana <ryansusana.com>
2018-06-09 15:42:10 +0200

    WE LIKE CUSTOMIZATIONS.
    Fixed Jongo default mapping not adding objectMapper features bug

--- core/src/main/java/com/elepy/dao/jongo/ElepyMapper.java
+++ core/src/main/java/com/elepy/dao/jongo/ElepyMapper.java
@@ -2,41 +2,52 @@ package com.elepy.dao.jongo;
 
 import com.elepy.dao.Crud;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jongo.Mapper;
 import org.jongo.ObjectIdUpdater;
 import org.jongo.marshall.Marshaller;
 import org.jongo.marshall.Unmarshaller;
+import org.jongo.marshall.jackson.JacksonEngine;
 import org.jongo.marshall.jackson.JacksonMapper;
+import org.jongo.marshall.jackson.bson4jackson.BsonModule;
+import org.jongo.marshall.jackson.bson4jackson.MongoBsonFactory;
+import org.jongo.marshall.jackson.configuration.AnnotationModifier;
+import org.jongo.marshall.jackson.configuration.Mapping;
+import org.jongo.marshall.jackson.configuration.PropertyModifier;
 import org.jongo.query.QueryFactory;
 
 public class ElepyMapper implements Mapper {
-    private final Mapper defaultMapper;
+    private final JacksonEngine jacksonEngine;
 
+    private final Mapper defaultMapper;
     private final ElepyIdUpdater elepyIdUpdater;
-    private final Crud crud;
 
     public ElepyMapper(Crud crud) {
-        this.crud = crud;
-        elepyIdUpdater = new ElepyIdUpdater(crud);
+        this.elepyIdUpdater = new ElepyIdUpdater(crud);
 
-        final JacksonMapper.Builder builder = new JacksonMapper.Builder();
+        final ObjectMapper mapper = new ObjectMapper(MongoBsonFactory.createFactory())
+                .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
+                .enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
-        builder.enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE);
-        builder.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        final Mapping mapping = new Mapping.Builder(mapper)
+                .registerModule(new BsonModule())
+                .addModifier(new PropertyModifier())
+                .addModifier(new AnnotationModifier())
+                .build();
 
-        builder.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-
-        defaultMapper = builder.build();
+        this.jacksonEngine = new JacksonEngine(mapping);
+        this.defaultMapper = new JacksonMapper.Builder().build();
     }
 
     @Override
     public Marshaller getMarshaller() {
-        return defaultMapper.getMarshaller();
+        return jacksonEngine;
     }
 
     @Override
     public Unmarshaller getUnmarshaller() {
-        return defaultMapper.getUnmarshaller();
+        return jacksonEngine;
     }
 
     @Override
