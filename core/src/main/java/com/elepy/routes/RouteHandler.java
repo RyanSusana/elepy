8a9c992a21270c7e4d505a1b7d1b0ced7a594e5b
RyanSusana <ryansusana.com>
2018-07-16 20:04:50 -0400

    getModelFromRequest

--- core/src/main/java/com/elepy/routes/RouteHandler.java
+++ core/src/main/java/com/elepy/routes/RouteHandler.java
@@ -3,13 +3,19 @@ package com.elepy.routes;
 import com.elepy.Elepy;
 import com.elepy.concepts.ObjectEvaluator;
 import com.elepy.dao.Crud;
-import com.elepy.exceptions.RestErrorMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import spark.Request;
 import spark.Response;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface RouteHandler<T> {
+
+    default T getModelFromRequest(Request request, ObjectMapper objectMapper, Class<T> clazz) throws IOException {
+
+        return objectMapper.readValue(request.body(), clazz);
+    }
+
     void handle(Request request, Response response, Crud<T> crud, Elepy elepy, List<ObjectEvaluator<T>> objectEvaluators, Class<T> clazz) throws Exception;
 }
