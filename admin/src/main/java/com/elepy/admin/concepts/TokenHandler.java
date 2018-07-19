eed883a43a43c71d0dbcc0d00855ce825393a61e
RyanSusana <ryansusana.com>
2018-07-19 13:50:00 -0400

    Tokens and Token Handler for login elsewhere

--- admin/src/main/java/com/elepy/admin/concepts/TokenHandler.java
+++ admin/src/main/java/com/elepy/admin/concepts/TokenHandler.java
@@ -3,12 +3,10 @@ package com.elepy.admin.concepts;
 import com.elepy.admin.models.Token;
 import com.elepy.admin.models.User;
 import com.elepy.admin.services.UserService;
-import com.elepy.exceptions.RestErrorMessage;
+import spark.Request;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.UUID;
+import java.nio.charset.Charset;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TokenHandler {
@@ -21,17 +19,31 @@ public class TokenHandler {
         this.userService = userService;
     }
 
-    public Token createToken(String username, String password) {
+    public Optional<Token> createToken(Request request) {
+
+        final Optional<String[]> credentials = basicCredentials(request);
+
+        if (!credentials.isPresent()) {
+            return Optional.empty();
+        }
+
+        final String username = credentials.get()[0];
+        final String password = credentials.get()[1];
+
+        return createToken(username, password);
+    }
+
+    public Optional<Token> createToken(String username, String password) {
         final Optional<User> login = userService.login(username, password);
 
         if (!login.isPresent()) {
-            throw new RestErrorMessage("Incorrect username or password");
+            return Optional.empty();
         }
 
         final Token token = new Token().setId(UUID.randomUUID().toString()).setCreationTime(System.currentTimeMillis()).setDuration(1000 * 60 * 60 * 24 * 7).setUser(login.get());
 
         tokens.add(token);
-        return token;
+        return Optional.of(token);
     }
 
     public void flushTokens() {
@@ -40,10 +52,33 @@ public class TokenHandler {
 
             final long maxTime = token.getCreationTime() + token.getDuration();
 
-            return currentTime > maxTime;
+            return currentTime < maxTime;
         }).collect(Collectors.toSet());
 
 
+    }
+
+    private Optional<String[]> basicCredentials(Request request) {
+        String header = request.headers("Authorization");
+
+        if (header == null || !(header = header.trim()).startsWith("Basic")) {
+            return Optional.empty();
+        }
+
+        header = header.replaceAll("Basic", "").trim();
+        final String[] strings = readBasicUsernamePassword(header);
+
+        if (strings.length != 2) {
+            return Optional.empty();
+        }
+        return Optional.of(strings);
+
+    }
+
+    private String[] readBasicUsernamePassword(String base64) {
+        String credentials = new String(Base64.getDecoder().decode(base64),
+                Charset.forName("UTF-8"));
+        return credentials.split(":", 2);
     }
 
 }
