f37f6e1a63a0be10562cd89ed19ce7bc29da6eca
RyanSusana <ryansusana.com>
2018-07-27 13:34:23 +0200

    Only the end of the slug

--- core/src/main/java/com/elepy/dao/MongoProvider.java
+++ core/src/main/java/com/elepy/dao/MongoProvider.java
@@ -12,6 +12,10 @@ public class MongoProvider<T> extends CrudProvider<T> {
     public Crud<T> crudFor(Class<T> type) {
         final RestModel model = type.getAnnotation(RestModel.class);
 
-        return new MongoDao<T>(elepy().getSingleton(DB.class), model.slug().replaceAll("/", ""), type);
+        final String slug = model.slug();
+
+        final String[] split = slug.split("/");
+
+        return new MongoDao<T>(elepy().getSingleton(DB.class), split[split.length-1], type);
     }
 }
