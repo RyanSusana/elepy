ec0bc8182e51594f97c833454179894803f313f0
RyanSusana <ryansusana.com>
2018-08-10 16:00:30 +0200

    SimpleCreate update

--- core/src/main/java/com/elepy/routes/SimpleCreate.java
+++ core/src/main/java/com/elepy/routes/SimpleCreate.java
@@ -15,13 +15,18 @@ public abstract class SimpleCreate<T> extends DefaultCreate<T> {
 
     @Override
     public void handle(Request request, Response response, Crud<T> dao, Elepy elepy, List<ObjectEvaluator<T>> objectEvaluators, Class<T> clazz) throws Exception {
-        super.handle(request, response, dao, elepy, objectEvaluators, clazz);
-        String body = request.body();
 
-        ObjectMapper objectMapper = elepy.getObjectMapper();
         try {
 
+            String body = request.body();
+
+            ObjectMapper objectMapper = elepy.getObjectMapper();
             T item = objectMapper.readValue(body, dao.getType());
+
+            beforeCreate(item, dao, elepy);
+
+            super.handle(request, response, dao, elepy, objectEvaluators, clazz);
+
 
             onCreate(item, dao, elepy);
 
@@ -30,6 +35,8 @@ public abstract class SimpleCreate<T> extends DefaultCreate<T> {
             throw new RestErrorMessage("MultipleCreate not supported with SimpleCreate");
         }
     }
+
+    public abstract void beforeCreate(T objectForCreation, Crud<T> crud, Elepy elepy);
 
     public abstract void onCreate(T createdObject, Crud<T> crud, Elepy elepy);
 }
