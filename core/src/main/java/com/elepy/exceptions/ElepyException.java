7ba85c80a263e1d40badd31d540ec53118463e18
Ryan Susana <ryansusana@live.com>
2018-12-23 17:46:35 +0100

    ElepyException extends ElepyErrorMessage

--- core/src/main/java/com/elepy/exceptions/ElepyException.java
+++ core/src/main/java/com/elepy/exceptions/ElepyException.java
@@ -1,7 +1,7 @@
 package com.elepy.exceptions;
 
-public class ElepyException extends RuntimeException {
+public class ElepyException extends ElepyErrorMessage {
     public ElepyException(String message) {
-        super(message);
+        super(message, 400);
     }
 }
