3f0c32ae4c0e45d6a9dd1eb82bbcf36cf8cdb9c2
Ryan Susana <ryansusana@live.com>
2019-01-10 14:32:09 +0100

    Test for crudFor

---
+++ core/src/test/java/com/elepy/di/ContextTest.java
@@ -0,0 +1,41 @@
+package com.elepy.di;
+
+import com.elepy.BaseTest;
+import com.elepy.Elepy;
+import com.elepy.concepts.Resource;
+import com.elepy.dao.Crud;
+import com.elepy.dao.jongo.MongoDao;
+import com.github.fakemongo.Fongo;
+import com.mongodb.DB;
+import com.mongodb.FongoDB;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ContextTest extends BaseTest {
+
+
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+
+
+    }
+
+    @Test
+    void testCrudFor() {
+        Elepy elepy = new Elepy();
+        Fongo fongo = new Fongo("test");
+        final FongoDB db = fongo.getDB("test");
+        MongoDao<Resource> mongoDao = new MongoDao<>(db, "resources", Resource.class);
+
+
+        elepy.attachSingleton(DB.class, db).addModel(Resource.class).onPort(1111).start();
+
+        Crud<Resource> crudFor = elepy.getContext().getCrudFor(Resource.class);
+
+        assertNotNull(crudFor);
+        assertTrue(crudFor instanceof MongoDao);
+    }
+}
