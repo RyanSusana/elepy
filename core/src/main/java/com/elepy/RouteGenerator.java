697d6cbd9b78de0a1cd05833615b9433cd794baa
Ryan Susana <ryansusana@live.com>
2019-01-13 17:39:44 +0100

    Removed attachments from routeGenerator

--- core/src/main/java/com/elepy/RouteGenerator.java
+++ core/src/main/java/com/elepy/RouteGenerator.java
@@ -3,7 +3,6 @@ package com.elepy;
 import com.elepy.concepts.ObjectEvaluator;
 import com.elepy.concepts.describers.StructureDescriber;
 import com.elepy.dao.Crud;
-import com.elepy.dao.CrudProvider;
 import com.elepy.models.AccessLevel;
 import com.elepy.utils.ClassUtils;
 import org.slf4j.Logger;
@@ -48,10 +47,7 @@ public class RouteGenerator<T> {
         try {
             List<ObjectEvaluator<T>> evaluators = restModel.getObjectEvaluators();
 
-            final Crud<T> dao = elepy.getCrudFor(clazz); //restModel.getCrudProvider().crudFor(clazz, elepy);
-
-            elepy.attachSingleton(Crud.class, restModel.getSlug(), dao);
-            elepy.attachSingleton(CrudProvider.class, restModel.getSlug(), restModel.getCrudProvider());
+            final Crud<T> dao = elepy.getCrudFor(clazz);
 
             setupFilters(restModel);
             if (!restModel.getCreateAccessLevel().equals(AccessLevel.DISABLED)) {
