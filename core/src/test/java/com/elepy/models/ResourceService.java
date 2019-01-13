97cc5294a55296434bf8213e74f3658acbc9f80e
Ryan Susana <ryansusana@live.com>
2019-01-13 18:11:09 +0100

    Added more descriptive ElepyConfigException

--- core/src/test/java/com/elepy/models/ResourceService.java
+++ core/src/test/java/com/elepy/models/ResourceService.java
@@ -1,9 +1,24 @@
 package com.elepy.models;
 
+import com.elepy.annotations.Inject;
+import com.elepy.concepts.ObjectEvaluator;
 import com.elepy.concepts.Resource;
+import com.elepy.dao.Crud;
+import com.elepy.dao.jongo.MongoDao;
+import com.elepy.di.ElepyContext;
 import com.elepy.routes.DefaultService;
+import spark.Request;
+import spark.Response;
+
+import java.util.List;
 
 public class ResourceService extends DefaultService<Resource> {
 
+    @Inject(tag = "/resources")
+    private MongoDao<Resource> crud;
 
+    @Override
+    public void handleFind(Request request, Response response, Crud<Resource> crud, ElepyContext elepy, List<ObjectEvaluator<Resource>> objectEvaluators, Class<Resource> clazz) throws Exception {
+        super.handleFind(request, response, crud, elepy, objectEvaluators, clazz);
+    }
 }
