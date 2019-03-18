7c72417dfd96f7ff6582e42b2171838bc19a2522
Ryan Susana <ryansusana@live.com>
2019-03-18 21:04:58 +0100

    Added SimpleDelete

--- core/src/main/java/com/elepy/routes/SimpleDelete.java
+++ core/src/main/java/com/elepy/routes/SimpleDelete.java
@@ -6,17 +6,23 @@ import com.elepy.exceptions.ElepyException;
 import com.elepy.http.HttpContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class DefaultDelete<T> implements DeleteHandler<T> {
-
+public abstract class SimpleDelete<T> implements DeleteHandler<T> {
     @Override
     public void handleDelete(HttpContext context, Crud<T> dao, ModelDescription<T> modelDescription, ObjectMapper objectMapper) throws Exception {
         Object paramId = context.modelId();
 
-        dao.getById(paramId).orElseThrow(() -> new ElepyException(String.format("No %s found", modelDescription.getName()), 404));
+        T itemToDelete = dao.getById(paramId).orElseThrow(() -> new ElepyException(String.format("No %s found", modelDescription.getName()), 404));
 
+        beforeDelete(itemToDelete, dao);
         dao.delete(paramId);
+        afterDelete(itemToDelete, dao);
 
         context.response().status(200);
         context.response().result("OK");
     }
+
+    public abstract void afterDelete(T itemToDelete, Crud<T> dao);
+
+    public abstract void beforeDelete(T itemToDelete, Crud<T> dao);
+
 }
