c603c2b43c1ab550c5307419f41a0da469747bb9
Ryan Susana <ryansusana@live.com>
2019-03-27 18:41:40 +0100

    Added the delegation methods HttpContext to Request/Response

--- core/src/main/java/com/elepy/http/HttpContext.java
+++ core/src/main/java/com/elepy/http/HttpContext.java
@@ -1,5 +1,10 @@
 package com.elepy.http;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Set;
+
 public interface HttpContext {
 
     /**
@@ -17,6 +22,167 @@ public interface HttpContext {
      */
     default Object modelId() {
         return request().modelId();
+    }
+
+    ////////// REQUEST
+    default String params(String param) {
+        return request().params(param);
+    }
+
+    default String requestMethod() {
+        return request().requestMethod();
+    }
+
+    default String scheme() {
+        return request().scheme();
+    }
+
+    default String host() {
+        return request().host();
+    }
+
+    default int port() {
+        return request().port();
+    }
+
+    default String url() {
+        return request().url();
+    }
+
+    default String ip() {
+        return request().ip();
+    }
+
+    default String body() {
+        return request().body();
+    }
+
+    default HttpServletRequest servletRequest() {
+        return request().raw();
+    }
+
+    default byte[] bodyAsBytes() {
+        return request().bodyAsBytes();
+    }
+
+    default String queryParams(String queryParam) {
+        return request().queryParams(queryParam);
+    }
+
+    default String queryParamOrDefault(String queryParam, String defaultValue) {
+        return request().queryParamOrDefault(queryParam, defaultValue);
+    }
+
+    default String headers(String header) {
+        return request().headers(header);
+    }
+
+    default <T> T attribute(String attribute) {
+        return request().attribute(attribute);
+    }
+
+    default Map<String, String> cookies() {
+        return request().cookies();
+    }
+
+    default String cookie(String name) {
+        return request().cookie(name);
+    }
+
+    default String uri() {
+        return request().uri();
+    }
+
+    default Session session() {
+        return request().session();
+    }
+
+    default String pathInfo() {
+        return request().pathInfo();
+    }
+
+    default String servletPath() {
+        return request().servletPath();
+    }
+
+    default String contextPath() {
+        return request().contextPath();
+    }
+
+    default Set<String> queryParams() {
+        return request().queryParams();
+    }
+
+    default Set<String> headers() {
+        return request().headers();
+    }
+
+    default String queryString() {
+        return request().queryString();
+    }
+
+    default Map<String, String> params() {
+        return request().params();
+    }
+
+    default String[] splat() {
+        return request().splat();
+    }
+
+    default void attribute(String attribute, Object value) {
+        request().attribute(attribute, value);
+    }
+
+
+    ////// RESPONSE
+
+    default void status(int statusCode) {
+        response().status(statusCode);
+    }
+
+    default int status() {
+        return response().status();
+    }
+
+    default void result(String body) {
+        response().result(body);
+    }
+
+
+    default String result() {
+        return response().result();
+    }
+
+    default HttpServletResponse servletResponse() {
+        return response().raw();
+    }
+
+    default void type(String type) {
+        response().type(type);
+    }
+
+    default String type() {
+        return response().type();
+    }
+
+    default void removeCookie(String name) {
+        response().removeCookie(name);
+    }
+
+    default void cookie(String name, String value) {
+        response().cookie(name, value);
+    }
+
+    default void cookie(String name, String value, int maxAge) {
+        response().cookie(name, value, maxAge);
+    }
+
+    default void redirect(String location) {
+        response().redirect(location);
+    }
+
+    default void redirect(String location, int httpStatusCode) {
+        response().redirect(location, httpStatusCode);
     }
 
 }
