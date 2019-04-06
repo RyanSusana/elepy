1d931bfb984e9c7ca2978dff01aa1a9834498ae4
Ryan Susana <ryansusana@live.com>
2019-04-07 00:21:55 +0200

    Added number safety to mongo filters

--- core/src/main/java/com/elepy/dao/jongo/MongoFilterTemplate.java
+++ core/src/main/java/com/elepy/dao/jongo/MongoFilterTemplate.java
@@ -1,8 +1,10 @@
 package com.elepy.dao.jongo;
 
 import com.elepy.dao.FilterableField;
+import com.elepy.exceptions.ElepyException;
 import com.elepy.models.FieldType;
 import com.elepy.models.NumberType;
+import com.elepy.utils.ClassUtils;
 import com.elepy.utils.DateUtils;
 
 import java.io.Serializable;
@@ -19,25 +21,29 @@ public class MongoFilterTemplate {
         this.field = field;
 
 
-        if (field.getFieldType().equals(FieldType.NUMBER)) {
+        try {
+            if (field.getFieldType().equals(FieldType.NUMBER)) {
 
-            NumberType numberType = NumberType.guessType(field.getField());
-            if (numberType.equals(NumberType.INTEGER)) {
-                this.value = Long.parseLong(value);
+                NumberType numberType = NumberType.guessType(field.getField());
+                if (numberType.equals(NumberType.INTEGER)) {
+                    this.value = Long.parseLong(value);
+                } else {
+                    this.value = Float.parseFloat(value);
+                }
+            } else if (field.getFieldType().equals(FieldType.DATE)) {
+
+                Date date = DateUtils.guessDate(value);
+
+                if (date == null) {
+                    this.value = value;
+                } else {
+                    this.value = date;
+                }
             } else {
-                this.value = Float.parseFloat(value);
-            }
-        } else if (field.getFieldType().equals(FieldType.DATE)) {
-
-            Date date = DateUtils.guessDate(value);
-
-            if (date == null) {
                 this.value = value;
-            } else {
-                this.value = date;
             }
-        } else {
-            this.value = value;
+        } catch (NumberFormatException e) {
+            throw new ElepyException(String.format("%s can only be compared to numbers", ClassUtils.getPrettyName(field.getField())));
         }
 
 
