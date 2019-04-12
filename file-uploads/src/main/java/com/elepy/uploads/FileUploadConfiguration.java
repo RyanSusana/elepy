feec25276e9b35a100b266617c6ebfe60717af81
Ryan Susana <ryansusana@live.com>
2019-04-13 00:54:22 +0200

    (enhancement) made ensureDirs happen on startup

--- file-uploads/src/main/java/com/elepy/uploads/FileUploadConfiguration.java
+++ file-uploads/src/main/java/com/elepy/uploads/FileUploadConfiguration.java
@@ -5,17 +5,15 @@ import com.elepy.ElepyPostConfiguration;
 import com.elepy.ElepyPreConfiguration;
 
 public class FileUploadConfiguration implements Configuration {
-    private final String rootFolderLocation;
+    private final DirectoryFileService directoryFileService;
 
     public FileUploadConfiguration(String rootFolderLocation) {
-        this.rootFolderLocation = rootFolderLocation;
+        directoryFileService = new DirectoryFileService(rootFolderLocation);
     }
 
     @Override
     public void before(ElepyPreConfiguration elepy) {
-        final DirectoryFileService directoryFileService = new DirectoryFileService(rootFolderLocation);
         elepy.withUploads(directoryFileService);
-
     }
 
     @Override
