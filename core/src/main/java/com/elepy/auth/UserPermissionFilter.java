99065debcdda734398d68a0e89ecc5ef016a973e
Ryan Susana <ryansusana@live.com>
2019-04-22 00:02:45 +0200

    (enhancement) made a better, more generic UserPermissionFilter

--- core/src/main/java/com/elepy/auth/UserPermissionFilter.java
+++ core/src/main/java/com/elepy/auth/UserPermissionFilter.java
@@ -5,25 +5,38 @@ import com.elepy.http.Filter;
 import com.elepy.http.HttpContext;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class UserPermissionFilter implements Filter {
 
-    private final Collection<String> permissions;
+    private final Collection<String> requiredPermissions;
 
     public UserPermissionFilter(Collection<String> permissions) {
-        this.permissions = permissions;
+        this.requiredPermissions = permissions;
     }
 
     @Override
     public void authenticate(HttpContext context) {
-        User user = context.attribute("user");
 
-        if (!permissions.isEmpty() && user == null) {
-            throw new ElepyException("You must be logged in to perform this procedure.", 401);
+        if (requiredPermissions.isEmpty()) {
+            return;
         }
+        Set<String> loggedInPermissions = new TreeSet<>();
+        User user = context.attribute("user");
+        if (user != null) {
+            if (user.getPermissions().contains("SUPER_USER")) {
+                return;
+            }
+            loggedInPermissions.addAll(user.getPermissions());
+        }
+        List<String> morePermissions = context.attribute("permissions");
 
-        if (!permissions.isEmpty() && !user.getPermissions().containsAll(permissions)) {
-            throw new ElepyException(String.format("%s is not authorized.", user.getUsername()), 401);
+        loggedInPermissions.addAll(morePermissions);
+
+        if (!requiredPermissions.isEmpty() && !loggedInPermissions.containsAll(requiredPermissions)) {
+            throw new ElepyException("User is not authorized.", 401);
         }
     }
 }
