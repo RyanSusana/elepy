ff250d4f0131ceb2af18ccad257465d8752f628f
Ryan Susana <ryansusana@live.com>
2019-04-27 23:45:50 +0200

    (enhancement) added MariaDB test and PostgreSQL test

--- test/src/test/java/com/elepy/test/e2e/user/slow/MariaDBUserEndToEndTest.java
+++ test/src/test/java/com/elepy/test/e2e/user/slow/MariaDBUserEndToEndTest.java
@@ -3,21 +3,22 @@ package com.elepy.test.e2e.user.slow;
 import com.elepy.Configuration;
 import com.elepy.database.DatabaseConfigurations;
 import com.elepy.test.e2e.user.ElepyUserEndToEndTest;
-import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-public class OracleXEUserEndToEndTest extends ElepyUserEndToEndTest {
+public class MariaDBUserEndToEndTest extends ElepyUserEndToEndTest {
 
     @Container
-    private static final OracleContainer ORACLE_CONTAINER = new OracleContainer();
+    private static final JdbcDatabaseContainer CONTAINER = new MariaDBContainer();
 
     @Override
     public Configuration configuration() {
         return DatabaseConfigurations.createTestContainerConfiguration(
-                ORACLE_CONTAINER,
-                "org.hibernate.dialect.Oracle10gDialect"
+                CONTAINER,
+                "org.hibernate.dialect.MariaDBDialect"
         );
     }
 }
--- test/src/test/java/com/elepy/test/e2e/user/slow/PostgreSQLUserEndToEndTest.java
+++ test/src/test/java/com/elepy/test/e2e/user/slow/PostgreSQLUserEndToEndTest.java
@@ -3,11 +3,22 @@ package com.elepy.test.e2e.user.slow;
 import com.elepy.Configuration;
 import com.elepy.database.DatabaseConfigurations;
 import com.elepy.test.e2e.user.ElepyUserEndToEndTest;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 public class PostgreSQLUserEndToEndTest extends ElepyUserEndToEndTest {
+
+    @Container
+    private static final JdbcDatabaseContainer CONTAINER = new PostgreSQLContainer();
+
     @Override
     public Configuration configuration() {
-
-        return DatabaseConfigurations.PostgreSQL;
+        return DatabaseConfigurations.createTestContainerConfiguration(
+                CONTAINER,
+                "org.hibernate.dialect.PostgreSQLDialect"
+        );
     }
 }
