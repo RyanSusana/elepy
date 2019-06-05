4b80335eeb2454808ae05bb05ef676b5e7144d7e
Ryan Susana <ryansusana@live.com>
2019-06-05 17:53:33 +0200

    (enhancement) added ElepyException helper methods

--- core/src/main/java/com/elepy/exceptions/ElepyException.java
+++ core/src/main/java/com/elepy/exceptions/ElepyException.java
@@ -12,4 +12,16 @@ public class ElepyException extends ElepyErrorMessage {
     public ElepyException(String message, int status, Throwable cause) {
         super(message, status, cause);
     }
+
+    public static ElepyException of(String message, int status, Throwable cause) {
+        return new ElepyException(message, status, cause);
+    }
+
+    public static ElepyException of(String message, int status) {
+        return new ElepyException(message, status);
+    }
+
+    public static ElepyException of(String message) {
+        return new ElepyException(message);
+    }
 }
