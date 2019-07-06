473cdae8ad3ef889c133da34b3510698313f2d54
Ryan Susana <ryansusana@live.com>
2019-07-06 22:00:31 +0200

    (enhancement) added helper method to HttpContextHandler

--- core/src/main/java/com/elepy/http/HttpContextHandler.java
+++ core/src/main/java/com/elepy/http/HttpContextHandler.java
@@ -1,7 +1,19 @@
 package com.elepy.http;
 
+import com.elepy.exceptions.ElepyException;
+
 @FunctionalInterface
 public interface HttpContextHandler {
 
     void handle(HttpContext context) throws Exception;
+
+    default void handleWithExceptions(HttpContext context) {
+        try {
+            handle(context);
+        } catch (ElepyException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ElepyException(e.getMessage(), 500, e);
+        }
+    }
 }
