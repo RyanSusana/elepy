c283b613f6e32bcc63c709df0113f88e63feb9a8
Ryan Susana <ryansusana@live.com>
2019-07-25 18:02:42 +0200

    (enhancement) added SingleView

---
+++ admin/src/main/java/com/elepy/admin/views/SingleView.java
@@ -0,0 +1,36 @@
+package com.elepy.admin.views;
+
+import com.elepy.admin.concepts.ModelView;
+import com.elepy.describers.Model;
+import com.elepy.http.Request;
+import com.mitchellbosecke.pebble.PebbleEngine;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleView implements ModelView {
+
+    private PebbleEngine engine = new PebbleEngine.Builder().build();
+
+    @Override
+    public String renderView(Request request, Model descriptor) {
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("currentDescriptor", descriptor);
+        Writer writer = new StringWriter();
+        try {
+            engine.getTemplate("admin-templates/model-views/single-model-view.peb").evaluate(writer, model);
+        } catch (IOException e) {
+            try {
+                writer.write(e.getMessage());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return writer.toString();
+
+    }
+}
