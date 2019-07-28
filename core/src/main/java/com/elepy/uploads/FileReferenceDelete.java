60c4dee94d0822fadf02c8159c2a36070cae990a
Ryan Susana <ryansusana@live.com>
2019-07-28 23:59:29 +0200

    (enhancement) made FileDelete be able to handle multiple references

--- core/src/main/java/com/elepy/uploads/FileReferenceDelete.java
+++ core/src/main/java/com/elepy/uploads/FileReferenceDelete.java
@@ -2,22 +2,30 @@ package com.elepy.uploads;
 
 import com.elepy.annotations.Inject;
 import com.elepy.dao.Crud;
-import com.elepy.routes.SimpleDelete;
+import com.elepy.describers.ModelContext;
+import com.elepy.exceptions.Message;
+import com.elepy.http.HttpContext;
+import com.elepy.routes.DeleteHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class FileReferenceDelete extends SimpleDelete<FileReference> {
+import java.io.Serializable;
+import java.util.Set;
+
+public class FileReferenceDelete implements DeleteHandler<FileReference> {
 
     @Inject
     private FileService fileService;
 
-
     @Override
-    public void beforeDelete(FileReference itemToDelete, Crud<FileReference> dao) {
-        fileService.deleteFile(itemToDelete.getName());
-    }
+    public void handleDelete(HttpContext context, Crud<FileReference> dao, ModelContext<FileReference> modelContext, ObjectMapper objectMapper) throws Exception {
+        Set<Serializable> paramIds = context.modelIds();
 
-    @Override
-    public void afterDelete(FileReference deletedItem, Crud<FileReference> dao) {
+        dao.getByIds(paramIds).forEach(fileReference -> {
+            fileService.deleteFile(fileReference.getUploadName());
+            dao.deleteById(fileReference.getUID());
+        });
 
+        context.result(Message.of("Successfully deleted items", 200));
     }
 
 }
