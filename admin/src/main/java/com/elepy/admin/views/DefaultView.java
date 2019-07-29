739b69c698f7db4b4cd1ab3b603150c834faee03
Ryan Susana <ryansusana@live.com>
2019-07-29 03:24:07 +0200

    (enhancement) added OfficialView helper class and FileView implementation

--- admin/src/main/java/com/elepy/admin/views/DefaultView.java
+++ admin/src/main/java/com/elepy/admin/views/DefaultView.java
@@ -1,36 +1,7 @@
 package com.elepy.admin.views;
 
-import com.elepy.admin.concepts.ModelView;
-import com.elepy.describers.Model;
-import com.elepy.http.Request;
-import com.mitchellbosecke.pebble.PebbleEngine;
-
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
-
-public class DefaultView implements ModelView<Object> {
-
-    private PebbleEngine engine = new PebbleEngine.Builder().build();
-
-    @Override
-    public String renderView(Request request, Model elepyModel) {
-
-        Map<String, Object> model = new HashMap<>();
-        model.put("model", elepyModel);
-        Writer writer = new StringWriter();
-        try {
-            engine.getTemplate("admin-templates/model-views/default-model-view.peb").evaluate(writer, model);
-        } catch (IOException e) {
-            try {
-                writer.write(e.getMessage());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return writer.toString();
-
+public class DefaultView extends OfficialView {
+    public DefaultView() {
+        super("elepy-default");
     }
 }
--- admin/src/main/java/com/elepy/admin/views/SingleView.java
+++ admin/src/main/java/com/elepy/admin/views/SingleView.java
@@ -1,36 +1,8 @@
 package com.elepy.admin.views;
 
-import com.elepy.admin.concepts.ModelView;
-import com.elepy.describers.Model;
-import com.elepy.http.Request;
-import com.mitchellbosecke.pebble.PebbleEngine;
+public class SingleView extends OfficialView {
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
-
-public class SingleView implements ModelView<Object> {
-
-    private PebbleEngine engine = new PebbleEngine.Builder().build();
-
-    @Override
-    public String renderView(Request request, Model elepyModel) {
-
-        Map<String, Object> model = new HashMap<>();
-        model.put("model", elepyModel);
-        Writer writer = new StringWriter();
-        try {
-            engine.getTemplate("admin-templates/model-views/single-model-view.peb").evaluate(writer, model);
-        } catch (IOException e) {
-            try {
-                writer.write(e.getMessage());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return writer.toString();
-
+    public SingleView() {
+        super("elepy-single");
     }
 }
