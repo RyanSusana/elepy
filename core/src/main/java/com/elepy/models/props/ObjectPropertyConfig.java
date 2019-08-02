57ef03cf0bf4f4bd0502b17320c08a73c2d2950f
Ryan Susana <ryansusana@live.com>
2019-08-02 03:46:07 +0200

    (enhancement) added @Object

--- core/src/main/java/com/elepy/models/props/ObjectPropertyConfig.java
+++ core/src/main/java/com/elepy/models/props/ObjectPropertyConfig.java
@@ -1,6 +1,7 @@
 package com.elepy.models.props;
 
 import com.elepy.annotations.Featured;
+import com.elepy.annotations.Object;
 import com.elepy.models.FieldType;
 import com.elepy.models.Property;
 import com.elepy.utils.ModelUtils;
@@ -22,14 +23,16 @@ public class ObjectPropertyConfig implements PropertyConfig {
 
     public static ObjectPropertyConfig of(AccessibleObject field) {
         Class<?> objectType = ReflectionUtils.returnTypeOf(field);
-        return of(objectType);
+        final Object annotation = field.getAnnotation(Object.class);
+        return of(objectType, annotation == null ? "" : annotation.name());
     }
 
-    public static ObjectPropertyConfig of(Class<?> objectType) {
+    public static ObjectPropertyConfig of(Class<?> objectType, String name) {
         final String featuredProperty = ReflectionUtils.searchForFieldWithAnnotation(objectType, Featured.class)
                 .map(ReflectionUtils::getPropertyName).orElse(null);
 
-        return new ObjectPropertyConfig(objectType.getSimpleName(), featuredProperty, ModelUtils.describeClass(objectType));
+        final String objectName = name.isBlank() ? objectType.getSimpleName() : name;
+        return new ObjectPropertyConfig(objectName, featuredProperty, ModelUtils.describeClass(objectType));
     }
 
     public static ObjectPropertyConfig of(Property property) {
