d1f93f3eefced0e37d6a1d604b0dbb4d86469e33
Ryan Susana <ryansusana@live.com>
2019-10-09 11:19:48 +0200

    (release) 2.2.7.3

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Exit on any command failure
+set -e
+
+# Login to NPM
 npm config set //registry.npmjs.org/:_authToken $NPM_OAUTH_TOKEN
+
+# Deploy to Maven Central
 mvn deploy --settings .travis.maven.settings.xml -Psign-artifacts -Ptravis -DskipTests=true -B -U -e
+
+# Publish to the NPM registry
 npm publish ./admin/src/main/resources/frontend
