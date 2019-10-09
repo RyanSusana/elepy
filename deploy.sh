1c94a7fab28822a68361cc7997ebe848120ab3d8
Ryan Susana <ryansusana@live.com>
2019-10-09 11:11:20 +0200

    (release) 2.2.7.3

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-mvn versions:set-property -Dproperty=revision -DnewVersion=$1
 npm config set //registry.npmjs.org/:_authToken $NPM_OAUTH_TOKEN
 mvn deploy --settings .travis.maven.settings.xml -Psign-artifacts -Ptravis -DskipTests=true -B -U -e
 npm publish ./admin/src/main/resources/frontend
