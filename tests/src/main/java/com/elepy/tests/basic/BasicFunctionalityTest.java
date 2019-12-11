bdc87ef9828dca3e055a0731e06b2b2b345517ca
Ryan Susana <ryansusana@live.com>
2019-12-11 11:32:45 +0100

    (enhancement) added tag

--- tests/src/main/java/com/elepy/tests/basic/BasicFunctionalityTest.java
+++ tests/src/main/java/com/elepy/tests/basic/BasicFunctionalityTest.java
@@ -29,6 +29,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Tag("integration")
 public abstract class BasicFunctionalityTest implements ElepyConfigHelper {
 
 
