49a719e3fbeb50f67c82d73c18d64974965818f3
Ryan Susana <ryansusana@live.com>
2020-01-14 14:16:52 +0100

    (bugfix) Fixed UI issues w images

--- admin/src/main/resources/frontend/src/components/modals/ImageLightBox.vue
+++ admin/src/main/resources/frontend/src/components/modals/ImageLightBox.vue
@@ -1,6 +1,6 @@
 <template>
     <div uk-lightbox>
-        <a :data-caption="caption" :href="src" class="uk-button uk-button-default">
+        <a :data-caption="caption" :href="src+'?height=250'" class="">
             <img :alt="alt" :data-src="src" :height="height" :width="width" uk-img>
         </a>
     </div>
