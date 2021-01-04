dfdb16e624e0cdb9f91efc3bde3cfdecd67bc0ed
Ryan Susana <ryansusana97@gmail.com>
2021-01-04 09:18:46 +0100

    (enhancement) further developed release pipeline

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm config set //registry.npmjs.org/:_authToken $NPM_OAUTH_TOKEN
 
 # Deploy to Maven Central
-mvn deploy --settings .travis.maven.settings.xml -Psign-artifacts -Ptravis -DskipTests=true -B -U -e
+mvn deploy  -Psign-artifacts -Ptravis -DskipTests=true -B -U -e
 
 # Publish to the NPM registry
 #npm publish ./admin/src/main/resources/frontend
